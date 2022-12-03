52260340193166f47c562ad8d7d98fb8b95d9220
datpd <phamducdat2402@gmail.com>
2022-12-04 04:30:36 +0700

    init

--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,11 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Output Management',
+            title: 'Caching',
         }),
     ],
     output: {
-        filename: '[name].bundle.js',
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
