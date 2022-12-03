07d210e6511fe3eb7b12a8f195512aca9d6ddfcd
datpd <phamducdat2402@gmail.com>
2022-12-04 02:29:05 +0700

    update

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,17 +1,19 @@
 import _ from 'lodash'
-import './style.css'
-import Icon from './icon.png'
+import printMe from "./print";
+
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
 
     element.innerHTML = _.join(['Hello', 'webpack'], '');
-    element.classList.add('hello');
 
-    const myIcon = new Image();
-    myIcon.src = Icon;
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
 
-    element.appendChild(myIcon);
+    element.appendChild(btn);
+
+
 
     return element;
 }
