7a6ed0a289410ec87c59edfbf9d9e5c66a75e0d4
datpd <phamducdat2402@gmail.com>
2022-12-04 02:12:36 +0700

    update

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,10 +1,20 @@
 const path = require('path');
 
 module.exports = {
+    mode:"development",
+
     entry:'./src/index.js',
     output: {
         filename:'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
-    mode:"development"
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
 }
