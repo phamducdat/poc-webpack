7b6e9c335bb036fd71ce34d72b5ab29c25655814
datpd <phamducdat2402@gmail.com>
2022-12-04 03:27:10 +0700

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,26 @@
 const path = require('path');
 
 module.exports = {
-    mode:'development',
+    mode: 'development',
+
     entry: {
-        index:'./src/index.js',
-        another:'./src/another-module.js',
+        index: {
+            import: './src/index.js',
+            dependOn: 'shared'
+        },
+        another: {
+            import: './src/another-module.js',
+            dependOn: 'shared',
+        },
+        shared: 'lodash',
     },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    optimization: {
+        splitChunks: {
+            chunks: "all"
+        }
+    }
 };
