9137b12b02ac929d5dd62fc8ad930c7c30ee4042
datpd <phamducdat2402@gmail.com>
2022-12-04 19:45:33 +0700

    init

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,11 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    mode:'development',
+    optimization: {
+        usedExports: true
+    }
 };
