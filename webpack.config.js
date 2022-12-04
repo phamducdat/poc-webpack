77f3f797c44cc561c00ed624f24b19226f3983a7
datpd <phamducdat2402@gmail.com>
2022-12-04 19:17:17 +0700

    update

--- webpack.config.js
+++ webpack.config.js
@@ -17,5 +17,21 @@ module.exports = {
             directory: path.join(__dirname, "build"),
         },
         port:3000
+    },
+
+    module: {
+        // exclude node_modules
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"],
+            },
+        ],
+    },
+    // pass all js files through Babel
+    resolve: {
+        extensions: ["*", ".js", ".jsx"],
     }
+
 }
