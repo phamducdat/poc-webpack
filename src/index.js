979c496b4f08931558f99f88a6e374b3d481b888
phamducdat <phamducdat2402@gmail.com>
2022-12-05 11:04:04 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import _ from 'lodash';
-import './style.css';
+
 function component() {
     const element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'webpack'], '')
-    element.classList.add('hello');
     return element
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -7,12 +7,4 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
-    },
 }
